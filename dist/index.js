87482e34be8981341f07fa7738fee8eafb5ff977
unknown <usuario@PUERTA18.CORP.AR>
2022-05-13 14:08:58 -0300

    con la carpeta dist

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Persona_1 = require("./classes/Persona");
+let date = new Persona_1.Persona(2022, "Dante");
+let fede = new Persona_1.Persona(2011, "federico fernandez");
+console.log(fede.cualEsTuNombre());
+console.log(fede.cualEsTuGeneracion());
+console.log(fede.cualEsTuEdad());
+console.log(fede.cualEsTuEdad());
